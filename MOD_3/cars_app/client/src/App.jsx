a614019631123bcbdc531805cbe2abe385742d2a
lana Holovatenko <lholovatenko@LABA.PSWAS.LOCAL>
2023-11-01 10:54:10 -0700

    second commit

---
+++ MOD_3/cars_app/client/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Cars from "./pages/Cars";
+import CreateCars from "./pages/CreateCars";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/create">
+          <button>Create New Car</button>
+        </Link>
+        <Link to="/cars">
+          <button>Cars</button>
+        </Link>
+        
+      
+      </nav>
+      <Routes>
+        <Route path="/cars" element={<Cars />}></Route>
+        <Route path="/create" element={<CreateCars />}></Route>
+  
+      </Routes>
+    </div>
+  );
+}
+export default App;
